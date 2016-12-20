c20a9845d6f5b66e604b59e6a1b844a07c3be5c9
USMC1941 <billie888chen@outlook.com>
2016-12-20 16:01:53 -0500

    Create ALStack.java

---
+++ Java Code/Centeno/linear/ALStack.java
@@ -0,0 +1,54 @@
+package linear;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+/*
+ * Stack using ArrayList
+ */
+public class ALStack<T> {
+
+	ArrayList<T> items;
+	
+	public ALStack () {
+		items = new ArrayList<T>();
+	}
+	
+	public ALStack (int initialCapacity) {
+		items = new ArrayList<T> (initialCapacity);
+	}
+	
+	public void push (T item) {
+		items.add(item);
+	}
+	
+	public T pop () {
+		if (items.size() == 0) {
+			throw new NoSuchElementException();
+		}
+		return items.remove(items.size()-1);
+	}
+	
+	public T peek () {
+		if (items.size() == 0) {
+			throw new NoSuchElementException();
+		}
+		return items.get(items.size()-1);
+	}
+	
+	public boolean isEmpty () {
+		return items.isEmpty();
+	}
+	
+	public int size () {
+		return items.size();
+	}
+	
+	public void clear() {
+		items.clear();
+	}
+	
+	public static void main (String[] args) {
+
+	}
+}
