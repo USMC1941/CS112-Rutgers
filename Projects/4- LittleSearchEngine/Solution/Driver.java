edb0bb87bacc2ef6588c6b04db174596e6bc98cf
Bill Chen <Bill Chen>
2017-03-14 11:41:13 -0400

    Added solutions for MST and LSE

---
+++ Projects/4- LittleSearchEngine/Solution/Driver.java
@@ -0,0 +1,21 @@
+package search;
+
+import java.io.*;
+
+public class Driver {
+
+    public static void main(String args[]) throws IOException {
+        String docsFile = "docs.txt";
+        String noiseWords = "noisewords.txt";
+
+        LittleSearchEngine searchEngine = new LittleSearchEngine();
+
+        searchEngine.makeIndex(docsFile, noiseWords);
+
+        String kw1 = "deep";
+        String kw2 = "world";
+
+        System.out.println(searchEngine.top5search(kw1, kw2));
+
+    }
+}
---
+++ Projects/5- Minimum Spanning Tree -MST/Solution/Driver.java
@@ -0,0 +1,30 @@
+package apps;
+
+import structures.Graph;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class Driver {
+
+    public static void main(String[] args) {
+        Graph graph = null;
+        try {
+            graph = new Graph("graph3.txt");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        //
+        PartialTreeList partialTreeList = MST.initialize(graph);
+        //
+        ArrayList<PartialTree.Arc> arcArrayList = MST.execute(partialTreeList);
+        //
+
+        for (int i = 0; i < arcArrayList.size(); i++) {
+            PartialTree.Arc anArcArrayList = arcArrayList.get(i);
+            System.out.println(anArcArrayList);
+        }
+    }
+}
