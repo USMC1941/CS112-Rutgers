7f32a1c27f74864951899f1e7d624aab15ae61e8
USMC1941 <billie888chen@outlook.com>
2017-09-19 18:57:32 -0400

    Readable

--- Java Code/Centeno/hash/HashUse.java
+++ Java Code/Centeno/hash/HashUse.java
@@ -49,26 +49,26 @@ public class HashUse {
 
 		/* Hash table where the key is an email (string) and the value is a Contact */
 		HashMap<String, Contact> friends = new HashMap<String, Contact>(500, 2.0f);
-		
+
 		Contact tmp = new Contact("Ana Paula", "Centeno", "anapaula@cs.rutgers.edu");
 		friends.put("Ana Paula", tmp); // insert into HT
-		
+
 		Contact tmp2 = new Contact("Sesh", "Venugopal", "venugopa@cs.rutgers.edu");
 		friends.put("Sesh", tmp2);
-		
+
 		System.out.println(friends.get("Sesh"));
-		
+
 		/* Hash table where */
 		HashMap<Point, Point> lines = new HashMap<Point,Point>();
 		Point A = new Point(3,5);
 		Point B = new Point(5,5);
 		Point C = new Point(3,2);
 		Point D = new Point(5,0);
-		
+
 		lines.put(A, B);
 		lines.put(C, D);
 		System.out.println("Key: " + A + " Value: " + lines.get(A));
 
-		
+
 	}
 }
