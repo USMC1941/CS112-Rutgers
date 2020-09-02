791d29ba77c98a57f895a3be0c7bf90c790fbea1
USMC1941 <16388993+USMC1941@users.noreply.github.com>
2020-09-02 19:29:38 -0400

    Format Trie.

--- Assignments/Trie/src/apps/TrieApp.java
+++ Assignments/Trie/src/apps/TrieApp.java
@@ -1,68 +1,74 @@
 package apps;
 
+import structures.Trie;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import structures.Trie;
-
 public class TrieApp {
 
-	static Scanner stdin = new Scanner(System.in);
-	static Trie trie = new Trie();
-	
-	public static void main(String[] args) 
-	throws IOException {
-		
-		System.out.print("(u)ser input, or (f)ile: ");
-		char inp = stdin.next().toLowerCase().charAt(0);
-		if (inp == 'u') {
-			getWordsFromUser();
-		} else {
-			getWordsFromFile();
-		}
-		completionList();
-		
-	}
+   static Scanner stdin = new Scanner(System.in);
+   static Trie    trie  = new Trie();
 
-	private static void completionList() {
-		System.out.print("\ncompletion list for (enter prefix, or 'quit'): ");
-		String prefix = stdin.next();
-		while (!"quit".equalsIgnoreCase(prefix)) {
-			ArrayList<String> matches = trie.completionList(prefix);
-			System.out.println(matches);
-			System.out.print("\ncompletion list for: ");
-			prefix = stdin.next();
-		}
-	}
-	
-	private static void getWordsFromUser() {
-		System.out.print("Enter word, 'quit' when done: ");
-		String word = stdin.next().trim().toLowerCase();
-		while (!"quit".equals(word)) {
-			trie.insertWord(word);
-			trie.print();
-			System.out.print("\nEnter word: ");
-			word = stdin.next().trim().toLowerCase();
-		}
-	}
-	
-	private static void getWordsFromFile() 
-	throws IOException {
-		System.out.print("Enter words file name => ");
-		String wordsFile = stdin.next();
-		Scanner sc = new Scanner(new File(wordsFile));
-		// words appear one per line in input file
-		// first line has number of words
-		int numWords = sc.nextInt();
-		String[] ret = new String[numWords];
-		for (int i=0; i < ret.length; i++) {
-			trie.insertWord(sc.next().trim());
-		}
-		sc.close();
-		trie.print();
-	}
-	
-	
+   public static void main(String[] args) throws IOException {
+
+      System.out.print("(u)ser input, or (f)ile: ");
+      char inp = stdin.next()
+                      .toLowerCase()
+                      .charAt(0);
+      if (inp == 'u') {
+         getWordsFromUser();
+      }
+      else {
+         getWordsFromFile();
+      }
+      completionList();
+
+   }
+
+   private static void completionList() {
+      System.out.print("\ncompletion list for (enter prefix, or 'quit'): ");
+      String prefix = stdin.next();
+      while (!"quit".equalsIgnoreCase(prefix)) {
+         ArrayList<String> matches = trie.completionList(prefix);
+         System.out.println(matches);
+         System.out.print("\ncompletion list for: ");
+         prefix = stdin.next();
+      }
+   }
+
+   private static void getWordsFromUser() {
+      System.out.print("Enter word, 'quit' when done: ");
+      String word = stdin.next()
+                         .trim()
+                         .toLowerCase();
+      while (!"quit".equals(word)) {
+         trie.insertWord(word);
+         trie.print();
+         System.out.print("\nEnter word: ");
+         word = stdin.next()
+                     .trim()
+                     .toLowerCase();
+      }
+   }
+
+   private static void getWordsFromFile() throws IOException {
+      System.out.print("Enter words file name => ");
+      String  wordsFile = stdin.next();
+      Scanner sc        = new Scanner(new File(wordsFile));
+      // words appear one per line in input file
+      // first line has number of words
+      int      numWords = sc.nextInt();
+      String[] ret      = new String[numWords];
+      for (int i = 0; i < ret.length; i++) {
+         trie.insertWord(sc.next()
+                           .trim());
+      }
+      sc.close();
+      trie.print();
+   }
+
+
 }
--- Assignments/Trie/src/structures/TrieNode.java
+++ Assignments/Trie/src/structures/TrieNode.java
@@ -5,71 +5,69 @@ package structures;
  * stored in an array of strings. The array of strings is the collection of
  * words that are indexed by the trie. Each node of the trie will have an
  * instance of Indexes.
- * 
+ *
  * Example: consider the words "have", "hit", "see", "data" stored in an
  * array in that order. Then, the substring "ave" in "have" will be indexed
  * by the triplet (0,1,3) ["have" is at position 0 in the array, 1 is the index
  * of character 'a' in "have" and 3 is the index of character 'e' in "have"]. 
  * Similarly, substring "ee" in the word "see" will be indexed by the triplet
  * (2,1,2). 
- * 
+ *
  * Substrings may be single characters, as in the first "a" in "data", 
  * which will be indexed by the triplet (3,1,1), or the second "a" in "data", 
  * which will be indexes by the triplet (3,3,3)
- * 
- * 
+ *
+ *
  * @author Sesh Venugopal
  *
  */
 class Indexes {
-	
-	/**
-	 * Index into the word collection array.
-	 */
-	int wordIndex;
-	
-	/**
-	 * Start index of substring in word.
-	 */
-	short startIndex;
-	
-	/**
-	 * End index of substring in word.
-	 */
-	short endIndex;
-	
-	/**
-	 * Initializes this instance with all indexes.
-	 * 
-	 * @param wordIndex Index of word in array of words
-	 * @param startIndex Starting index of substring
-	 * @param endIndex Ending index of substring
-	 */
-	public Indexes(int wordIndex, short startIndex, short endIndex) {
-		this.wordIndex = wordIndex;
-		this.startIndex = startIndex;
-		this.endIndex = endIndex;
-	}
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString() {
-		return "(" + wordIndex + "," + startIndex + "," + endIndex + ")";
-	}
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	public boolean equals(Object o) {
-		if (o == null || !(o instanceof Indexes)) {
-			return false;
-		}
-		Indexes oi = (Indexes)o;
-		return wordIndex == oi.wordIndex &&
-				startIndex == oi.startIndex &&
-				endIndex == oi.endIndex;
-	}
+
+   /**
+    * Index into the word collection array.
+    */
+   int wordIndex;
+
+   /**
+    * Start index of substring in word.
+    */
+   short startIndex;
+
+   /**
+    * End index of substring in word.
+    */
+   short endIndex;
+
+   /**
+    * Initializes this instance with all indexes.
+    *
+    * @param wordIndex Index of word in array of words
+    * @param startIndex Starting index of substring
+    * @param endIndex Ending index of substring
+    */
+   public Indexes(int wordIndex, short startIndex, short endIndex) {
+      this.wordIndex = wordIndex;
+      this.startIndex = startIndex;
+      this.endIndex = endIndex;
+   }
+
+   /* (non-Javadoc)
+    * @see java.lang.Object#toString()
+    */
+   public String toString() {
+      return "(" + wordIndex + "," + startIndex + "," + endIndex + ")";
+   }
+
+   /* (non-Javadoc)
+    * @see java.lang.Object#equals(java.lang.Object)
+    */
+   public boolean equals(Object o) {
+      if (!(o instanceof Indexes)) {
+         return false;
+      }
+      Indexes oi = (Indexes) o;
+      return wordIndex == oi.wordIndex && startIndex == oi.startIndex && endIndex == oi.endIndex;
+   }
 }
 
 /**
@@ -77,45 +75,45 @@ class Indexes {
  * - an Indexes instance, pointing to the substring that is held at that node
  * - the first child node
  * - the sibling node
- * 
+ *
  * @author Sesh Venugopal
  *
  */
 public class TrieNode {
 
-	/**
-	 * Substring held at this node (could be a single character)
-	 */
-	Indexes substr;
-	
-	/**
-	 * First child of this node
-	 */
-	TrieNode firstChild;
-	
-	/**
-	 * Sibling of this node
-	 */
-	TrieNode sibling;
-	
-	/**
-	 * Initializes this trie node with substring, first child, and sibling
-	 * 
-	 * @param substr Substring held at this node
-	 * @param firstChild First child of this node
-	 * @param sibling Sibling of this node
-	 */
-	public TrieNode(Indexes substr, TrieNode firstChild, TrieNode sibling) {
-		this.substr = substr;
-		this.firstChild = firstChild;
-		this.sibling = sibling;
-	}
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString() {
-		return substr.toString();
-	}
-	
+   /**
+    * Substring held at this node (could be a single character)
+    */
+   Indexes substr;
+
+   /**
+    * First child of this node
+    */
+   TrieNode firstChild;
+
+   /**
+    * Sibling of this node
+    */
+   TrieNode sibling;
+
+   /**
+    * Initializes this trie node with substring, first child, and sibling
+    *
+    * @param substr Substring held at this node
+    * @param firstChild First child of this node
+    * @param sibling Sibling of this node
+    */
+   public TrieNode(Indexes substr, TrieNode firstChild, TrieNode sibling) {
+      this.substr = substr;
+      this.firstChild = firstChild;
+      this.sibling = sibling;
+   }
+
+   /* (non-Javadoc)
+    * @see java.lang.Object#toString()
+    */
+   public String toString() {
+      return substr.toString();
+   }
+
 }
